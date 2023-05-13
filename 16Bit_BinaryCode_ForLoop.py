1b929833bd8e180401a21ab58d65319249975a61
BhuvaneshRs <132887722+BhuvaneshRs@users.noreply.github.com>
2023-05-13 11:37:58 +0530

    16Bit_BinaryCode_ForLoop

---
+++ 16Bit_BinaryCode_ForLoop.py
@@ -0,0 +1,26 @@
+print("This are binary numbers 16 Bit")
+for i in range(0,2):
+  for j in range(0,2):
+    for k in range(0,2):
+      for l in range(0,2):
+        print(i,j,k,l)
+        
+        
+#Output:
+This are binary numbers 16 Bit
+0 0 0 0
+0 0 0 1
+0 0 1 0
+0 0 1 1
+0 1 0 0
+0 1 0 1
+0 1 1 0
+0 1 1 1
+1 0 0 0
+1 0 0 1
+1 0 1 0
+1 0 1 1
+1 1 0 0
+1 1 0 1
+1 1 1 0
+1 1 1 1
