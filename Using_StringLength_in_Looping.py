298ecb0e11a19ebb858926e455ea81562423a9bb
BhuvaneshRs <132887722+BhuvaneshRs@users.noreply.github.com>
2023-05-13 11:32:26 +0530

    Using_StringLength_in_Looping

---
+++ Using_StringLength_in_Looping.py
@@ -0,0 +1,17 @@
+x="Bhuvanesh"
+for i in range(0,len(x)):
+  print(x)
+else:
+  print("Loop done")
+  
+#Output:
+Bhuvanesh
+Bhuvanesh
+Bhuvanesh
+Bhuvanesh
+Bhuvanesh
+Bhuvanesh
+Bhuvanesh
+Bhuvanesh
+Bhuvanesh
+Loop done
